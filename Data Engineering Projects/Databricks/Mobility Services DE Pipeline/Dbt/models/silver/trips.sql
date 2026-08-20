bb4eac8798762c4b208aa1075a3a36f7fc09275d
siddharthchandurkar-git <146814455+siddharthchandurkar-git@users.noreply.github.com>
2026-08-20 12:30:58 +0530

    Add files via upload

---
+++ Data Engineering Projects/Databricks/Mobility Services DE Pipeline/Dbt/models/silver/trips.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'trip_id'
+    )
+}}
+
+
+{% set cols = ['trip_id','vehicle_id','customer_id','driver_id','trip_start_time','trip_end_time','distance_km','fare_amount','last_updated_timestamp'] %}
+
+select 
+    {% for col in cols%}
+        {{ col }}
+            {% if not loop.last %}
+                ,
+            {% endif %}
+    {%endfor%}
+from {{source("source_bronze","trips")}}
+{% if is_incremental() %}
+where
+    last_updated_timestamp > (select coalesce(max(last_updated_timestamp),'1900-01-01') from {{ this }} )
+{% endif %}
